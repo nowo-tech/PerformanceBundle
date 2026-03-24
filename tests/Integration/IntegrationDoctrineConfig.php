37f25fe625dc61df463f6ba5f3ab6f6a79a75c66
GitHub Action <action@github.com>
2026-03-24 22:41:22 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- tests/Integration/IntegrationDoctrineConfig.php
+++ tests/Integration/IntegrationDoctrineConfig.php
@@ -6,6 +6,8 @@ namespace Nowo\PerformanceBundle\Tests\Integration;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+use const PHP_VERSION_ID;
+
 /**
  * Loads Doctrine integration config; enables native lazy objects on PHP 8.4+ (see doctrine_native_lazy_php84.yaml).
  */
@@ -14,7 +16,7 @@ final class IntegrationDoctrineConfig
     public static function load(LoaderInterface $loader, string $packagesConfigDir): void
     {
         $loader->load($packagesConfigDir . '/doctrine.yaml');
-        if (\PHP_VERSION_ID >= 80400) {
+        if (PHP_VERSION_ID >= 80400) {
             $loader->load($packagesConfigDir . '/doctrine_native_lazy_php84.yaml');
         }
     }
